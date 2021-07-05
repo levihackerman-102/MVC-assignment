ca70c9b77e703218451cfac1b2c1af3ac4959d5c
LeviHackerman102 <anshulspidy@gmail.com>
2021-07-05 15:03:09 +0530

    Initializatin script completed

---
+++ initialize.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+git clone git@github.com:levihackerman-102/MVC-assignment.git
+
+# import db using phpmyadmin and the schema.sql file provided
+
+composer install
+curl -s https://getcomposer.org/installer | php
+composer dump-autoload
+
+# see sample vhost conf file
+sudo nano /etc/apache2/sites-available/<domain name>.conf
+sudo nano /etc/hosts
+# add 127.0.0.1      <domain name>
+
+sudo a2ensite <domain name>
+sudo a2enmod rewrite
+sudo apachectl configtest
+sudo service apache2 reload
+
+# now visit your domain name to view site
